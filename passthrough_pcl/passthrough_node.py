eab100206b5164000e0956f500c998aefd34a788
Eric <ephan3@gatech.edu>
2021-06-28 15:51:00 -0400

    added function for passthrough

--- passthrough_pcl/passthrough_node.py
+++ passthrough_pcl/passthrough_node.py
@@ -21,17 +21,40 @@ class passthrough_pcl(Node):
 
         points = read_points(msg,field_names=['x','y','z'])
         
-        filtered_points = []
-        for point in points:
-            if (point[0] > 2):
-                filtered_points.append(point)
-        
+        filtered_points = self.passthrough(pointcloud_data=points, y_thresh=2)
         
         filtered_cloud = create_cloud_xyz32(header=header, points=filtered_points)
 
         self.publisher_.publish(filtered_cloud)
         
+    
+    """
+    Filter data from pointcloud.
 
+    @param points: The point cloud points.
+    @type  points: list of iterables, i.e. one iterable for each point, with the
+                   elements of each iterable being the values of the fields for 
+                   that point (in the same order as the fields parameter)
+    @param x_thresh: The minimum value for x to be included in filtered cloud.
+    @type  x_thresh: float
+    @param y_thresh: The minimum value for y to be included in filtered cloud.
+    @type  y_thresh: float
+    @param z_thresh: The minimum value for z to be included in filtered cloud.
+    @type  z_thresh: float
+    
+    @return: The filtered point cloud.
+    @rtype:  L{sensor_msgs.msg.PointCloud2}
+    """
+    
+    def passthrough(self, pointcloud_data, x_thresh=-999, y_thresh=-999, z_thresh=-999):
+        filtered_points = []
+
+        for point in pointcloud_data:
+            if (point[0] > y_thresh and point[1] > x_thresh and point[2] > z_thresh):
+                filtered_points.append(point)
+                
+        return filtered_points
+        
                 
 
             
